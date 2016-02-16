0d21374778aa0ffa7ef51da2cbee9cfed76ade72
Ulfu <din_dr_elvis@live.se>
2016-02-16 19:30:06 +0100

    Added some more steering functions.

--- Car.ino
+++ Car.ino
@@ -1,14 +1,14 @@
 const int leftMotorPin1 = 2; // connected to pin 7 on the H-bridge
 const int leftMotorPin2 = 3; // connected to pin 2 on the H-bridge
-const int rightMotorPin1 = 5;
-const int rightMotorPin2 = 6;
+const int rightMotorPin1 = 5; // connected to pin 10 on the H-bridge
+const int rightMotorPin2 = 6; // connected to pin 15 on the H-bridge
 const int enableLeftPin = 9;   // connected to pin 1 on the H-bridge
-const int enableRightPin = 10;
+const int enableRightPin = 10;  // connected to pin 9 on the H-bridge
 
 const int onButton = 4;
 
-const int pingPin = 8;
-const int echoPin = 7;
+const int pingPin = 8; // connected to trigger pin
+const int echoPin = 7;  // connected to echo pin
 
 int on = false;
 int buttonLastState = false;
@@ -70,9 +70,48 @@ void backGoLeft() {
     digitalWrite(leftMotorPin1, HIGH);
     digitalWrite(leftMotorPin2, LOW);
     analogWrite(enableLeftPin, 500);
-
+    
+    delay(20);
 }
 
+void backGoRight() {
+    // PWM the enable pin to vary the speed
+    // change the direction the motor spins by talking
+    // to the control pins on the H-Bridge
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, HIGH);
+    analogWrite(enableLeftPin,50);
+
+    digitalWrite(rightMotorPin1, HIGH);
+    digitalWrite(rightMotorPin2, LOW);
+    analogWrite(enableRightPin, 500);
+
+    delay(20);
+}
+
+void goLeft() {
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, HIGH);
+    digitalWrite(enableRightPin, HIGH);
+
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, HIGH);
+    analogWrite(enableLeftPin, 500);
+    
+    delay(1);
+}
+
+void goRight() {
+    digitalWrite(rightMotorPin1, LOW);
+    digitalWrite(rightMotorPin2, HIGH);
+    analogWrite(enableRightPin, 500);
+
+    digitalWrite(leftMotorPin1, LOW);
+    digitalWrite(leftMotorPin2, HIGH);
+    digitalWrite(enableLeftPin, HIGH);
+    
+    delay(1);
+}
 void forward() {
     digitalWrite(rightMotorPin1, LOW);
     digitalWrite(rightMotorPin2, HIGH);
@@ -80,6 +119,18 @@ void forward() {
 
     digitalWrite(leftMotorPin1, LOW);
     digitalWrite(leftMotorPin2, HIGH);
+    digitalWrite(enableLeftPin, HIGH);
+    
+    delay(1);
+}
+
+void backwards() {
+    digitalWrite(rightMotorPin1, HIGH);
+    digitalWrite(rightMotorPin2, LOW);
+    digitalWrite(enableRightPin, HIGH);
+
+    digitalWrite(leftMotorPin1, HIGH);
+    digitalWrite(leftMotorPin2, LOW);
     digitalWrite(enableLeftPin, HIGH);
     
     delay(1);
